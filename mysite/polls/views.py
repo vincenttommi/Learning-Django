dc25ef59c5e2dfe63452ca1208dd777e6a86542e
vincenttommi <vincenttommi@gmail.com>
2023-09-02 06:23:12 +0300

    adding form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models  import  Question
-from django.shortcuts  import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+# from  django.shortcuts import Http404
+from .models  import Question
+
 
 def index(request):
     lastest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -13,11 +13,8 @@ def index(request):
 
 
 def  detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return  render(request, "polls/detail.html", {"question" : question})
+    question = get_object_or_404(Question,pk=question_id)
+    return  render(request, "polls/detail.html",{"question":question})   
     
         
     
