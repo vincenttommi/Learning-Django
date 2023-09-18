9a9aef1afd0f86562b13ac3919113ec9416639ea
vincenttommi <vincenttommi@gmail.com>
2023-09-18 09:11:48 +0300

    adding a css file

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() -datetime.timedelta(days=1)
+     now = timezone.now()
+     return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
 
 
@@ -27,6 +28,10 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     #this line of code defines an integer field
     # named "votes" in the current Django model.
+    
+    
+    
+    
     
     
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results",views.Resultsview.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote.as_view(), name="vote"),
     
 ]
